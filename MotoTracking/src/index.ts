f5389c399d510941eee6c43a8d5ffa9e382def12
Stephen Buchanan <webtvmaster@gmail.com>
2024-12-17 16:12:13 +0100

    pointing correctly to id 0

--- MotoTracking/src/index.ts
+++ MotoTracking/src/index.ts
@@ -4,8 +4,9 @@ import rc from "rc";
 import { SerialPort } from "serialport";
 import { ReadlineParser } from "@serialport/parser-readline";
 import { getLogger } from "log4js";
-import { BROKER_DEFAULTS, InputPlug, TetherAgent } from "tether-agent";
+import { BROKER_DEFAULTS, decode, InputPlug, TetherAgent } from "tether-agent";
 import { remap } from "@anselan/maprange";
+import { TrackedPoint } from "./types";
 
 const appName = defaults.appName;
 
@@ -31,18 +32,32 @@ const main = async () => {
     serialport.write(0 + "\n");
   });
 
-  // const agent = await TetherAgent.create("trackingToMotor", {
-  //   brokerOptions: BROKER_DEFAULTS.nodeJS,
-  // });
-  // const trackingInput = await InputPlug.create(agent, "smoothedRemappedPoints");
+  const agent = await TetherAgent.create("trackingToMotor", {
+    brokerOptions: BROKER_DEFAULTS.nodeJS,
+  });
+  const trackingInput = await InputPlug.create(agent, "smoothedRemappedPoints");
+  trackingInput.on("message", (payload) => {
+    const subjects = decode(payload) as TrackedPoint[];
+    for (const u of subjects) {
+      const { id, x, y } = u;
+      const angleTo = toDegreees(Math.atan2(y, x));
 
-  setInterval(() => {
-    const angle = remap(Math.random(), [0, 1], [-180, 180]).toFixed(1);
-    logger.debug("sending random angle", angle);
-    serialport.write(angle + "\n");
-  }, 3000);
+      if (id === 0) {
+        logger.debug({ id, angleTo });
+        serialport.write(-angleTo + "\n");
+      }
+    }
+  });
+
+  // setInterval(() => {
+  //   const angle = remap(Math.random(), [0, 1], [-180, 180]).toFixed(1);
+  //   logger.debug("sending random angle", angle);
+  //   serialport.write(angle + "\n");
+  // }, 3000);
 };
 
 // ================================================
 // Kick off main process here
 main();
+
+const toDegreees = (radians: number) => (radians * 180) / Math.PI;
