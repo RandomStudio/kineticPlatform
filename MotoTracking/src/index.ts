ebeefefd02d0334ef6c82305886f2c73b4e23f42
Stephen Buchanan <webtvmaster@gmail.com>
2025-01-31 10:34:34 +0100

    random works, can use bearing

--- MotoTracking/src/index.ts
+++ MotoTracking/src/index.ts
@@ -40,55 +40,51 @@ const main = async () => {
   if (config.testMode) {
     setInterval(() => {
       const angle = remap(Math.random(), [0, 1], [-180, 180]).toFixed(1);
-      logger.debug("sending random angle", angle);
+      logger.debug("sending RANDOM angle", angle);
       serialport.write(angle + "\n");
     }, 3000);
-
-    logger.warn(
-      `Please move motor to Zero point. System will start in ${
-        config.waitToZero / 1000
-      }s ...`
-    );
-
-    await awaitDelay(config.waitToZero);
-
-    serialport.on("open", () => {
-      logger.warn("Setting zero point!");
-      serialport.write(0 + "\n");
-    });
-
-    let currentTargetId = 0;
-
-    const agent = await TetherAgent.create("trackingToMotor", {
-      brokerOptions: BROKER_DEFAULTS.nodeJS,
-    });
-    const trackingInput = await InputPlug.create(
-      agent,
-      "smoothedTrackedPoints"
-    );
-    trackingInput.on("message", (payload) => {
-      const subjects = decode(payload) as TrackedPoint[];
-      for (const u of subjects) {
-        const { id, x, y, bearing } = u;
-        const angleTo =
-          bearing !== undefined ? bearing : toDegreees(Math.atan2(y, x));
-
-        if (id === currentTargetId) {
-          logger.debug({ currentTargetId, id, angleTo });
-          serialport.write(-angleTo + "\n");
-        }
-      }
-    });
-
-    const targetInput = await InputPlug.create(agent, "subjectTargetIds");
-    targetInput.on("message", (payload) => {
-      const m = decode(payload) as number;
-      if (currentTargetId !== m) {
-        logger.info("Setting new target ID:", m);
-        currentTargetId = m;
-      }
-    });
   }
+  logger.warn(
+    `Please move motor to Zero point. System will start in ${
+      config.waitToZero / 1000
+    }s ...`
+  );
+
+  await awaitDelay(config.waitToZero);
+
+  serialport.on("open", () => {
+    logger.warn("Setting zero point!");
+    serialport.write(0 + "\n");
+  });
+
+  let currentTargetId = 0;
+
+  const agent = await TetherAgent.create("trackingToMotor", {
+    brokerOptions: BROKER_DEFAULTS.nodeJS,
+  });
+  const trackingInput = await InputPlug.create(agent, "smoothedTrackedPoints");
+  trackingInput.on("message", (payload) => {
+    const subjects = decode(payload) as TrackedPoint[];
+    for (const u of subjects) {
+      const { id, x, y, bearing } = u;
+      const angleTo =
+        bearing !== undefined ? -bearing : toDegreees(Math.atan2(y, x));
+
+      if (id === currentTargetId) {
+        logger.debug({ currentTargetId, id, angleTo });
+        serialport.write(-angleTo + "\n");
+      }
+    }
+  });
+
+  const targetInput = await InputPlug.create(agent, "subjectTargetIds");
+  targetInput.on("message", (payload) => {
+    const m = decode(payload) as number;
+    if (currentTargetId !== m) {
+      logger.info("Setting new target ID:", m);
+      currentTargetId = m;
+    }
+  });
 };
 
 // ================================================
